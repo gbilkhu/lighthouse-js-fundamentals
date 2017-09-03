92519b4b7914bb5f0af01339d6f0142b49c703c8
Garry Bilkhu <garry.bilkhu@gmail.com>
2017-09-03 08:09:06 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensiles", "glasses", "cups", "cooking utensiles"];
+
+console.log("Kitchen stuff to pack");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
